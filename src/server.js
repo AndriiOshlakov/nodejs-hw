ce1c11cf6ba71e8574522583c42c97e152a80e65
Andrii <andreyoshlakov18051986@gmail.com>
2025-10-03 18:38:24 +0300

    done

--- src/server.js
+++ src/server.js
@@ -7,15 +7,19 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import { errors } from 'celebrate';
+import authRoutes from './routes/authRoutes.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT ?? 3030;
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 app.use(logger);
 
 app.use(notesRoutes);
+app.use(authRoutes);
 
 app.use(notFoundHandler);
 
